141245d4c5f25ed8c7d2b64f396db0d4c0126374
simonmkay <simonmkay@yahoo.com>
2014-04-22 17:41:59 -0400

    Updates
    Admin stuff is working!

---
+++ Truck/Truck/Controllers/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Truck.Controllers
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string password { get; set; }
+
+    }
+}
